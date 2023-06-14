9bb7fb56b9a4b9c233b4aa3739dad8e1c9acfd1d
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-14 06:49:14 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+ function saturdayFun(activity = "roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+ }
+
+ 
+
+
+
+ function mondayWork(job="go to the office"){
+    return `This Monday, I will ${job}.`
+ };
+
+
+ function wrapAdjective(special="*"){
+    return (function( string){
+        return `You are ${special}${string}${special}!`
+    })
+ }
+
+ wrapAdjective("a dedicated programmer", "*")
